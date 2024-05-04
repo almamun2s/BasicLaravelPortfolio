41ab16d64ae0e7fc01b457e1350a4b0efe98e281
almamun2s <mamun2social@gmail.com>
2024-05-05 01:42:02 +0600

    theme changed in recover password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,51 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('admin.main_layout')
+@section('title', 'Recover Password')
+@section('body_class', 'auth-body-bg')
+
+@section('page_content')
+    <div class="bg-overlay"></div>
+    <div class="wrapper-page">
+        <div class="container-fluid p-0">
+            <div class="card">
+                <div class="card-body">
+
+                    @include('auth.logo')
+
+                    <h4 class="text-muted text-center font-size-18"><b>Reset Password</b></h4>
+
+                    <div class="p-3">
+                        <form class="form-horizontal mt-3" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+
+                            <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                Enter Your <strong>E-mail</strong> and instructions will be sent to you!
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <div class="col-xs-12">
+                                    <input class="form-control block mt-1 w-full" type="email"autofocus placeholder="Email" id="email" name="email">
+                                    @error('email')
+                                        <p class="text-danger mt-2 mb-0">{{ $message }} </p>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group pb-2 text-center row mt-3">
+                                <div class="col-12">
+                                    <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Send
+                                        Email</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end cardbody -->
+            </div>
+            <!-- end card -->
+        </div>
+        <!-- end container -->
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    <!-- end -->
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,17 +10,8 @@
             <div class="card">
                 <div class="card-body">
 
-                    <div class="text-center mt-4">
-                        <div class="mb-3">
-                            <a href="index.html" class="auth-logo">
-                                <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30"
-                                    class="logo-dark mx-auto" alt="">
-                                <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30"
-                                    class="logo-light mx-auto" alt="">
-                            </a>
-                        </div>
-                    </div>
-
+                    @include('auth.logo')
+                    
                     <h4 class="text-muted text-center font-size-18"><b>Login</b></h4>
 
                     <div class="p-3">
