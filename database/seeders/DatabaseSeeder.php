a8b9910611dd961ce47b1ffd1f067196710d9482
Abdullah Almamun <mamun2social@gmail.com>
2024-07-03 15:13:55 +0600

    DB seed added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\About;
+use App\Models\Footer;
+use App\Models\HomeSlides;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'username'=> 'admin',
+            'email' => 'admin@portfolios.com',
+        ]);
+        HomeSlides::create();
+        About::create();
+        Footer::create();
     }
 }
